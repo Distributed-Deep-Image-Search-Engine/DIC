961b631cc87db18b424a8d841de96bad8bde8f4c
absingh31 <absinghemail@gmail.com>
2018-03-12 23:46:35 +0530

    writing reducer code for hadoop

---
+++ amazon/scraper/reducer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""reducer.py"""
+
+from operator import itemgetter
+import urllib.request as ur
+import sys
+
+current_asin = None
+current_count = 0
+word = None
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    asin, data = line.split('\t')
+    print(asin)
+    print(data)
+#     # this IF-switch only works because Hadoop sorts map output
+#     # by key (here: word) before it is passed to the reducer
+#     if current_asin == asin:
+#         continue
+#     else:
+#         if asin:
+#             urls = data[2]
+#             # write result to STDOUT
+#             print '%s\t%s' % (current_word, current_count)
+#         current_count = count
+#         current_word = word
+
+# # do not forget to output the last word if needed!
+# if current_word == word:
+#     print '%s\t%s' % (current_word, current_count)
+
+
+
+
+
+def download(urls=['http://www.gunnerkrigg.com//comics/00000001.jpg']):
+    images = []
+    for url in urls:
+        try:
+            image = ur.urlopen(url).read()
+        except urllib.HTTPError, e:
+            print(e.code)
+        except urllib.URLError, e:
+            print(e.args)
+        images.append(image)
+    return images
+
+def store_image(images):
+    pass
