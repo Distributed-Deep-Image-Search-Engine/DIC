37ceac587482d34a1f55af7639ad51445c14eb09
amandavinci <aman@amandavinci.me>
2018-03-13 08:49:23 +0530

    all json; json everything

--- amazon/scraper/reducer.py
+++ amazon/scraper/reducer.py
@@ -1,50 +1,13 @@
 #!/usr/bin/env python3
 """reducer.py"""
 
-from operator import itemgetter
-import urllib.request as ur
 import sys
-
-current_asin = None
-current_count = 0
-word = None
-
-# input comes from STDIN
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-
-    print("\n\nREDUCER INPUT\n\n {}".format(line))
-    
-    # parse the input we got from mapper.py
-    asin, data = line.split('|')
-
-    print("\n\nREDUCER SPLIT\n\n")
-    print(asin)
-    print()
-    print(data)
-
-#     # this IF-switch only works because Hadoop sorts map output
-#     # by key (here: word) before it is passed to the reducer
-#     if current_asin == asin:
-#         continue
-#     else:
-#         if asin:
-#             urls = data[2]
-#             # write result to STDOUT
-#             print '%s\t%s' % (current_word, current_count)
-#         current_count = count
-#         current_word = word
-
-# # do not forget to output the last word if needed!
-# if current_word == word:
-#     print '%s\t%s' % (current_word, current_count)
+import json
+import base64
+import urllib.request as ur
 
 
-
-
-
-def download(urls=['http://www.gunnerkrigg.com//comics/00000001.jpg']):
+def download(urls):
     images = []
     for url in urls:
         try:
@@ -56,5 +19,36 @@ def download(urls=['http://www.gunnerkrigg.com//comics/00000001.jpg']):
         images.append(image)
     return images
 
+def encode_images(images):
+    json_images = []
+    for image in images:
+        json_image = base64.b64encode(image)
+        json_images.append(json_image)
+    json_images = json_dump(json_images) 
+    return json_images
+
 def store_image(images):
     pass
+
+def main():
+    current_asin = None
+    # input is a json dump
+    for line in sys.stdin:
+        # clean dump
+        line = line.strip()    
+        # parse the input we got from mapper.py
+        asin, data_dump = line.split('\t')
+        # get back the dict format
+        data = json.loads(data_dump)
+        # if we have duplicate asins
+        if current_asin == asin:
+           continue;
+        else:
+            links = data['links']
+            images = download(links)
+            json_images = encode_images(images)
+            current_asin = asin
+            print("{}\t{}{}".format(asin, data_dump, json_images))
+
+if __name__ == '__main__':
+    main()
