cebe58af46329b9e53c64821baecb1f00540e8eb
anmolbhalla <anmol9599@live.com>
2018-03-13 15:00:10 +0530

    Regex bug fix in scrapper

--- amazon/scraper/scraper.py
+++ amazon/scraper/scraper.py
@@ -15,7 +15,7 @@ class ProductData:
         self.soup = BeautifulSoup((self.reqt).text, "lxml")   # making soup
 
     def get_images(self):
-        im = self.soup.find(text=re.compile('\\colorImages\\b'))  # finding product images
+        im = self.soup.find(text=re.compile('\\bcolorImages\\b'))  # finding product images
 
         if im is None:
             return None
@@ -46,7 +46,7 @@ class ProductData:
         return None
 
     def get_asin(self):
-        asin = self.soup.find(text=re.compile('\\mediaAsin\\b'))
+        asin = self.soup.find(text=re.compile('\\bmediaAsin\\b'))
 
         if asin is None:
             return None
@@ -86,16 +86,3 @@ class ProductData:
     def __del__(self):
         # just a deconstructor
         return 0
-# '''
-# ['B01G1E3R3A']\t
-# ['','']
-# ["Alan Jones Men's Cotton Printed T-Shirt"]
-# ['Clothing&Accessories', 'Men', 'T-Shirts&Polos', 'LongSleeveTops']
-# ['Fabric - 100% Cotton', 'Luxurious cotton fabric for superior comfort', 'Collection AJC SS17, Authentic AJC® logo label', 'Care Instruction - Machine Wash or Hand Wash', 'Proudly made in India']
-# '''
-
-
-
-
-
-# "large":"https://images-na.ssl-images-amazon.com/images/I/51rrBALx3yL.jpg","main":{"https://images-na.ssl-images-amazon.com/images/I/91pnP8dhCUL._UY445_.jpg":[445,333],"https://images-na.ssl-images-amazon.com/images/I/91pnP8dhCUL._UY500_.jpg":[500,374],"https://images-na.ssl-images-amazon.com/images/I/91pnP8dhCUL._UY550_.jpg":[550,412],"https://images-na.ssl-images-amazon.com/images/I/91pnP8dhCUL._UY606_.jpg":[606,454],"https://images-na.ssl-images-amazon.com/images/I/91pnP8dhCUL._UY679_.jpg":[679,508]},"variant":"MAIN","lowRes":null},{"hiRes":"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UL1500_.jpg","thumb":"https://images-na.ssl-images-amazon.com/images/I/51r54JjOUdL._SR38,50_.jpg","large":"https://images-na.ssl-images-amazon.com/images/I/51r54JjOUdL.jpg","main":{"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UY445_.jpg":[445,321],"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UY500_.jpg":[500,361],"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UY550_.jpg":[550,397],"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UY606_.jpg":[606,438],"https://images-na.ssl-images-amazon.com/images/I/91ynOfDH9-L._UY679_.jpg":[679,490]},"variant":"PT01","lowRes":null},{"hiRes":"https://images-na.ssl-images-amazon.com/images/I/917dAmfWdbL._UL1500_.jpg","thumb":"https://images-na.ssl-images-amazon.com/images/I/51aSayhFitL._SR38,50_.jpg","large":"https://images-na.ssl-images-amazon.com/images/I/51aSayhFitL.jpg","main":{"https://images-na.ssl-images-amazon.com/images/I/917dAmfWdbL._UY445_.jpg":[445,326],"https://images-na.ssl-images-amazon.com/images/I/917dAmfWdbL._UY500_.jpg":[500,366],"https://images-na.ssl-images-amazon.com/images/I/917dAmfWdbL._UY550_.jpg":[550,403],"https://images-na.ssl-images-amazon.com/images/I/917dAmfWdbL._UY606_.jpg"
