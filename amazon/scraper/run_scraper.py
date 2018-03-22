e1a107e32bfb0d2fe1735d8566fa831968aabbf9
absingh31 <absinghemail@gmail.com>
2018-03-23 04:55:17 +0530

    keyboard_exception

--- amazon/scraper/run_scraper.py
+++ amazon/scraper/run_scraper.py
@@ -4,27 +4,33 @@ from collections import OrderedDict
 
 df = pd.DataFrame()
 
-file_name = 'give the desired file_name'
-
-for line in open(file_name):
-	url = line.strip()
-	
-	# Making the class object
-	prod = ProductData(url)
-
-	my_dict = OrderedDict()
-	my_dict['Asin'] = prod.get_asin()
-	my_dict['Title'] = prod.get_title()
-	my_dict['Categories'] = prod.get_category()
-	my_dict['Images_Links'] = prod.get_images()
-	my_dict['MetaData'] = prod.meta_data()
-
-	df = df.append(my_dict, ignore_index = True)
-
-# TO pickle the data into specified file
-df.to_pickle('dataframe_data')
+file_name = 'Mens-fashion-product-links'
 
 
-#TO load the dataframe from the given file in the exact right format
-dfs = pd.read_pickle('dataframe_data')
-print(dfs.iloc[0][2])
+try:
+	count = 0 
+	for line in open(file_name):
+		url = line.strip()
+		
+		# Making the class object
+		prod = ProductData(url)
+
+		my_dict = OrderedDict()
+		my_dict['Asin'] = prod.get_asin()
+		my_dict['Title'] = prod.get_title()
+		my_dict['Categories'] = prod.get_category()
+		my_dict['Images_Links'] = prod.get_images()
+		my_dict['MetaData'] = prod.meta_data()
+
+		df = df.append(my_dict, ignore_index = True)
+		count = count + 1
+		print(count) 
+
+except KeyboardInterrupt:
+	df.to_pickle('dataframe_data')
+	dfs = pd.read_pickle('dataframe_data')
+	print(dfs)
+
+# #TO load the dataframe from the given file in the exact right format
+# dfs = pd.read_pickle('dataframe_data')
+# print(dfs.iloc[0][2])
